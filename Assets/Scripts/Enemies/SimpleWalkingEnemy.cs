7a08d9c128419b8d41ee50ae05d877a4752ae795
pontakornth <pontakorn61@gmail.com>
2023-12-16 23:55:36 +0700

    Physics but wrong

--- Assets/Scripts/Enemies/SimpleWalkingEnemy.cs
+++ Assets/Scripts/Enemies/SimpleWalkingEnemy.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 
 namespace Enemies
 {
+    [RequireComponent(typeof(Rigidbody2D)), RequireComponent(typeof(TouchingDirections)),
+     RequireComponent(typeof(Damageable))]
     public class SimpleWalkingEnemy : MonoBehaviour
     {
+        private Rigidbody2D _rb;
         public float walkSpeed = 3f;
+        public float walkAcceleration = 5f;
+        public float maxSpeed = 3f;
+        public float walkStopRate = 0.05f;
 
         public enum WalkableDirection
         {
@@ -12,7 +18,7 @@ namespace Enemies
             Right
         }
 
-        private WalkableDirection _walkDirection;
+        private WalkableDirection _walkDirection = WalkableDirection.Left;
 
         public WalkableDirection WalkDirection
         {
@@ -31,6 +37,22 @@ namespace Enemies
             }
         }
 
+        private void FlipDirection()
+        {
+            if (WalkDirection == WalkableDirection.Left)
+            {
+                WalkDirection = WalkableDirection.Right;
+            }
+            else if (WalkDirection == WalkableDirection.Right)
+            {
+                WalkDirection = WalkableDirection.Left;
+            }
+            else
+            {
+                Debug.Log("Something went wrong");
+            }
+        }
+
         private Vector2 WalkDirectionVector
         {
             get
@@ -45,17 +67,41 @@ namespace Enemies
                 }
             }
         }
-        
-        // Start is called before the first frame update
-        void Start()
+
+        private TouchingDirections _touchingDirections;
+
+        public void OnHit(int damage, Vector2 knockback)
         {
-        
+            _rb.velocity = new(knockback.x, _rb.velocity.y + knockback.y);
+        }
+
+        // Start is called before the first frame update
+        void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+            _touchingDirections = GetComponent<TouchingDirections>();
         }
 
         // Update is called once per frame
-        void Update()
+        void FixedUpdate()
         {
-        
+            if (_touchingDirections.IsGround && _touchingDirections.IsOnWall)
+            {
+                Debug.Log("Flip direction");
+                FlipDirection();
+            }
+
+            if ( /*CanMove &&*/ _touchingDirections.IsGround)
+            {
+                float xVelocity =
+                    Mathf.Clamp(_rb.velocity.x + (walkAcceleration * WalkDirectionVector.x * Time.deltaTime), -maxSpeed,
+                        maxSpeed);
+                _rb.velocity = new Vector2(xVelocity, _rb.velocity.y);
+            }
+            else
+            {
+                _rb.velocity = new Vector2(Mathf.Lerp(_rb.velocity.x, 0, walkStopRate), _rb.velocity.y);
+            }
         }
     }
 }
