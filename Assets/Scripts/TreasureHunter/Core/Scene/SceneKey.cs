2cdd2d4f5063f0639baa70cfe99b33b48f99fb9e
pontakornth <pontakorn61@gmail.com>
2024-01-31 15:48:10 +0700

    Add level exit and entrance to normal cave

--- Assets/Scripts/TreasureHunter/Core/Scene/SceneKey.cs
+++ Assets/Scripts/TreasureHunter/Core/Scene/SceneKey.cs
@@ -7,6 +7,7 @@ namespace TreasureHunter.Core.Scene
         // Level
         public const string THE_ENTRANCE = "TheEntrance";
         public const string ABNORMAL_CAVE = "AbnormalCave";
+        public const string NORMAL_CAVE = "NormalCave";
 
     }
 }
