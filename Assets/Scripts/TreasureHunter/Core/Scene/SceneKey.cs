d41e93af27f56413da166316bdac129e0ffaeba3
pontakornth <pontakorn61@gmail.com>
2024-02-07 18:04:28 +0700

    Make Weird Space

--- Assets/Scripts/TreasureHunter/Core/Scene/SceneKey.cs
+++ Assets/Scripts/TreasureHunter/Core/Scene/SceneKey.cs
@@ -8,6 +8,7 @@ namespace TreasureHunter.Core.Scene
         public const string THE_ENTRANCE = "TheEntrance";
         public const string ABNORMAL_CAVE = "AbnormalCave";
         public const string NORMAL_CAVE = "NormalCave";
+        public const string WEIRD_SPACE = "WeirdSpace";
 
     }
 }
