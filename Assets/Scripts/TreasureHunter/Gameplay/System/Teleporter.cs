ad88879341a34c20998f39a37a19459a535304f3
SainTurDaY27 <poom52344@gmail.com>
2024-04-02 22:04:10 +0700

    Make teleporter can teleport to other map

--- Assets/Scripts/TreasureHunter/Gameplay/System/Teleporter.cs
+++ Assets/Scripts/TreasureHunter/Gameplay/System/Teleporter.cs
@@ -1,4 +1,7 @@
 using System;
+using TreasureHunter.Core.Scene;
+using TreasureHunter.Core.State.GameState;
+using TreasureHunter.Core.UI;
 using TreasureHunter.Gameplay.Player;
 using UnityEngine;
 
@@ -7,14 +10,48 @@ namespace TreasureHunter.Gameplay.System
     [RequireComponent(typeof(Collider2D))]
     public class Teleporter : MonoBehaviour
     {
+        public bool isTeleportToOtherMap;
+        public string sceneName;
         public Vector2 teleportDestination = Vector2.zero;
+
         private void OnTriggerEnter2D(Collider2D other)
         {
-            if (!other.CompareTag("Player")) return;
-            var playerController = GetPlayerController(other);
-            if (playerController == null) return;
-            playerController.SetTeleportDestination(teleportDestination);
-            
+            if (!isTeleportToOtherMap)
+            {
+                if (!other.CompareTag("Player")) return;
+                var playerController = GetPlayerController(other);
+                if (playerController == null) return;
+                playerController.SetTeleportDestination(teleportDestination);
+            }
+            else if (isTeleportToOtherMap)
+            {
+                var oldDamageable = other.GetComponent<Damageable>();
+                var oldPlayerController = other.GetComponent<PlayerController>();
+
+                // lock player movement
+                oldDamageable.LockVelocity = true;
+                oldPlayerController.ResetVelocity();
+
+                UIManager.Instance.FadeSceneTransition(true, () =>
+                {
+                    GameManager.Instance.PauseGame(true);
+                    var oldHealth = oldDamageable.Health;
+                    GameSceneManager.Instance.GoToScene(sceneName, () =>
+                    {
+                        GameManager.Instance.PauseGame(false);
+                        UIManager.Instance.FadeSceneTransition(false);
+                        var player = FindObjectOfType<PlayerController>();
+                        player.transform.position = teleportDestination;
+
+                        var newDamageable = player.GetComponent<Damageable>();
+                        newDamageable.Health = oldHealth;
+
+                        var gameState = GameStateManager.Instance.CurrentState;
+                        gameState.OnStateIn();
+                        newDamageable.LockVelocity = false;
+                    });
+                });
+            }
         }
 
         private void OnTriggerExit2D(Collider2D other)
